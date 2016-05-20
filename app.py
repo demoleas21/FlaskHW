f8c3116da7a9cd8dbc917cd5eefd1bd79a4e8840
demoleas21 <andrew.demoleas@gmail.com>
2016-05-20 00:45:26 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+"""Flask Hello World"""
+
+"""import the Flask class from the flask package"""
+from flask import Flask
+
+"""create the application object"""
+app = Flask(__name__)
+
+"""use decorators to link the function to a url"""
+@app.route("/")
+@app.route("/hello")
+def hello_world():
+    return "Hello, World!"
+
+"""start the development server using the run() method"""
+if __name__ == "__main__":
+    app.run()
+
